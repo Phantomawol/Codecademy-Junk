14ff0dab0674ddaae99d97ef347387281b9bd8c0
Awol <austinhamre@gmail.com>
2017-08-11 14:33:46 -0500

    Add files via upload
    A 3.5 version of Codecademy since they do Python in 2.7 I converted it for myself to have.

---
+++ Stringformat%.py
@@ -0,0 +1,7 @@
+#Taken from Codecademy since this is 3.5 and they teach it in 2.7
+
+name = input("What is your name? ")
+quest = input("What is your quest? ")
+color = input("What is your favorite color? ")
+
+print("So your name is %s, your quest is %s, and your favorite color is %s? Noice")
